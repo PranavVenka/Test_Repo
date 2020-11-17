16ec5b5be44940bbd19d8f0b74b13af8847dadc7
PranavVenka <74613051+PranavVenka@users.noreply.github.com>
2020-11-17 11:38:05 -0500

    Adding my-python file to repo

---
+++ my-python.py
@@ -0,0 +1,16 @@
+file_contnets = open("h.txt","r")
+
+
+result = {}
+a = file_contents.split()
+for word in a:
+    if word in uninteresting_words:
+        pass
+    else:
+        for letter in word:
+            if letter in punctuations:
+                letter.replace(punctuations, "")
+        if word not in result.keys():
+            result[word]=0
+        else:
+            result[word]+=1
